a33af5b6541e444192b804f4fac1efa8c26f9350
dz1to4ka <117407093+illiaBabak@users.noreply.github.com>
2025-10-04 15:07:35 +0200

    Added table for waiters

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,5 +1,57 @@
+import {
+  ColumnDef,
+  getCoreRowModel,
+  useReactTable,
+  flexRender,
+} from "@tanstack/react-table";
 import { JSX } from "react";
 
-export const Table = (): JSX.Element => {
-  return <div>Table</div>;
+type Props<T> = {
+  data: T[];
+  columns: ColumnDef<T>[];
+};
+
+export const Table = <T,>({ data, columns }: Props<T>): JSX.Element => {
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  return (
+    <table className="w-full outline-1 outline-gray-300 rounded-lg">
+      <thead>
+        {table.getHeaderGroups().map((headerGroup) => (
+          <tr key={`table-headerGroup-${headerGroup.id}`}>
+            {headerGroup.headers.map((header) => (
+              <th
+                className="bg-gray-100 text-gray-500 p-4 pl-6 pr-2 text-left"
+                key={`table-header-${header.id}`}
+              >
+                {flexRender(
+                  header.column.columnDef.header,
+                  header.getContext()
+                )}
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+
+      <tbody>
+        {table.getRowModel().rows.map((row) => (
+          <tr className="hover:bg-neutral-100" key={`table-row-${row.id}`}>
+            {row.getVisibleCells().map((cell) => (
+              <td
+                className="p-4 pl-6 pr-2 text-left"
+                key={`table-cell-${cell.id}`}
+              >
+                {flexRender(cell.column.columnDef.cell, cell.getContext())}
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 };
--- src/pages/Admin/index.tsx
+++ src/pages/Admin/index.tsx
@@ -4,14 +4,57 @@ import { SideBar } from "src/components/SideBar";
 import { useGetWaiters } from "src/api/waiters";
 import { Loader } from "src/components/Loader";
 import { Table } from "src/components/Table";
+import { Waiter } from "src/types";
+import { ColumnDef } from "@tanstack/react-table";
+import { useSearchParams } from "react-router";
+
+const waitersColumns: ColumnDef<Waiter>[] = [
+  {
+    header: "Name",
+    accessorKey: "name",
+  },
+  {
+    header: "Surname",
+    accessorKey: "surname",
+  },
+  {
+    header: "Email",
+    accessorKey: "email",
+  },
+  {
+    header: "Phone Number",
+    accessorKey: "phone_number",
+  },
+  {
+    header: "Address",
+    accessorKey: "address",
+  },
+];
 
 export const Admin = (): JSX.Element => {
   const { data: waiters, isLoading: isLoadingWaiters } = useGetWaiters();
 
+  const [searchParams] = useSearchParams();
+
+  const selectedTable = searchParams.get("table") ?? "Waiters";
+
+  const isWaitersTable = selectedTable === "Waiters";
+
   return (
     <div className="flex flex-col h-screen">
       <Header />
-      <SideBar />
+      <div className="flex items-start h-full">
+        <SideBar />
+        <div className="w-full px-7 pt-4">
+          <h2 className="text-2xl font-bold tracking-wide mb-4">
+            {selectedTable}
+          </h2>
+
+          {isWaitersTable && waiters && (
+            <Table data={waiters} columns={waitersColumns} />
+          )}
+        </div>
+      </div>
       {isLoadingWaiters && <Loader />}
     </div>
   );
